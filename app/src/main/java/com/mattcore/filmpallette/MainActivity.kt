5b6bf4f5257fbb0eea4070265e9411842e0dd3c9
mmirzynski <mmirzynski@gbox.pl>
2022-07-03 14:05:16 +0100

    Palette is being generated Added caption functionality RevealActivity.kt now shows the palette, sent path and caption

--- app/src/main/java/com/mattcore/filmpallette/MainActivity.kt
+++ app/src/main/java/com/mattcore/filmpallette/MainActivity.kt
@@ -25,10 +25,13 @@ import com.royrodriguez.transitionbutton.TransitionButton
 class MainActivity : AppCompatActivity() {
     companion object {
         const val REQUEST_CODE_PICTURE_PICKER = 1012
+        const val KEY_PATH = "path"
+        const val KEY_CAPTION = "caption"
     }
 
     private lateinit var binding: ActivityMainBinding
     var bitmap: Bitmap? = null
+    private lateinit var picturePath: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,19 +48,9 @@ class MainActivity : AppCompatActivity() {
 
         binding.generateButton.setOnClickListener {
             if (bitmap != null) {
-                val button = it as TransitionButton
-                button.startAnimation()
-
-                Handler().postDelayed({
-                    val palette = Palette.from(bitmap!!).generate()
-                    button.stopAnimation(
-                        TransitionButton.StopAnimationStyle.EXPAND
-                    ) {
-                        val intent = Intent(this, RevealActivity::class.java)
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-                        startActivity(intent)
-                    }
-                }, 2000)
+                transitionToActivity(it)
+            } else if (binding.caption.text.isNullOrEmpty()) {
+                Toast.makeText(this, "Provide a caption", Toast.LENGTH_LONG).show()
             } else {
                 Toast.makeText(this, "Load an image first", Toast.LENGTH_LONG).show()
             }
@@ -66,21 +59,41 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        val button =  binding.generateButton
-        //TODO get button to initial state
+        val button = binding.generateButton
+
         button.clearAnimation()
+        button.text = "Generate"
+        button.isClickable = true
     }
 
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_PICTURE_PICKER) {
             val uri: Uri = data?.data!!
+            picturePath = uri
             bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
             binding.picturePreview.setImageURI(uri)
             binding.noPictureIcon.visibility = View.GONE
         } else if (resultCode == ImagePicker.RESULT_ERROR) {
             Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun transitionToActivity(view: View) {
+        val button = view as TransitionButton
+        button.startAnimation()
+        Handler().postDelayed({
+            button.stopAnimation(
+                TransitionButton.StopAnimationStyle.EXPAND
+            ) {
+                val intent = Intent(this, RevealActivity::class.java)
+                intent.putExtra(KEY_PATH, picturePath)
+                intent.putExtra(KEY_CAPTION, binding.caption.text.toString())
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+            }
+        }, 2000)
     }
 }
--- app/src/main/java/com/mattcore/filmpallette/RevealActivity.kt
+++ app/src/main/java/com/mattcore/filmpallette/RevealActivity.kt
@@ -1,16 +1,36 @@
 package com.mattcore.filmpallette
 
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import androidx.palette.graphics.Palette
 import com.mattcore.filmpallette.databinding.ActivityRevealBinding
 
 class RevealActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRevealBinding
+    private lateinit var path: Uri
+    private lateinit var caption: String
+    private lateinit var palette: Palette
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRevealBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        path = intent.extras!!.get(MainActivity.KEY_PATH) as Uri
+        caption = intent.extras!!.getString(MainActivity.KEY_CAPTION) as String
+        val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, path)
+        palette = Palette.from(bitmap).maximumColorCount(4).generate()
+
+        binding.caption.text = "Sent caption: $caption"
+        binding.path.text = "Bitmap path: ${path.path}"
+
+        binding.color1.setBackgroundColor(palette.swatches[0].rgb)
+        binding.color2.setBackgroundColor(palette.swatches[1].rgb)
+        binding.color3.setBackgroundColor(palette.swatches[2].rgb)
+        binding.color4.setBackgroundColor(palette.swatches[3].rgb)
+
     }
 }
